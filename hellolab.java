0ceb38e575eb62cd16b4271514e3d376429035e7
unknown <rusialeksis@ufl.edu>
2022-03-10 18:34:34 -0500

    changed to substraction

--- hellolab.java
+++ hellolab.java
@@ -2,6 +2,6 @@ public class hellolab {
     public static void main( String[] args){
         int x=2;
         int y=2;
-        System.out.println(x+y);
+        System.out.println(x-y);
     }
 }
